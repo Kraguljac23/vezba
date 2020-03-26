f2590df75f90d75139bcec04bc686d2a465f0567
Kraguljac23 <61576481+Kraguljac23@users.noreply.github.com>
2020-03-26 21:41:57 +0100

    ostale funkcije

--- rerna.cpp
+++ rerna.cpp
@@ -5,7 +5,7 @@ trenutnoStanje = ISKLJUCENA;
 temperatura = MIN_TEMPERATURA:
 }
 
-rerna::ukljuci(){
+bool rerna::ukljuci(){
   bool uspesno;
   
   if(trenutnoStanje == ISKLJUCENA){
@@ -17,7 +17,8 @@ rerna::ukljuci(){
   }
   return uspesno;
   }
-rerna::iskljuci(){
+
+bool rerna::iskljuci(){
   bool uspesno;
   
   if(trenutnoStanje == ukljucena){
@@ -28,4 +29,62 @@ rerna::iskljuci(){
     uspesno = false;
   }
   return uspesno;
+}
+
+  bool rerna::popravi(){
+    bool uspesno;
+    
+  if(trenutnoStanje == POKVARENA){
+    trenutnoStanje = ISKLJUCENA;
+    temperatura = MIN_TEMPERATURA;
+    uspesno = true;
+  }else{
+    uspesno = false;
+  }
+  return uspesno;
+}
+
+bool rerna::pokvari(){
+  bool uspesno;
+  
+if(trenutnoStanje != ISKLJUCENA){
+  trenutno stanje = POKVARENA;
+  temperatura = MIN_TEMPERATURA;
+  uspesno = true;
+}else{
+  uspesno = false;
+}
+  return uspesno;
+}
+
+bool rerna::povecajTemperaturu(){
+  bool uspesno;
+  
+ if(trenutnoStanje = UKLJUCENA && temperatura + KORAK_ZAGREVANJA <= MAX_TEMPERATURA){
+   temperatura = temperatura + KORAK_ZAGREVANJA;
+   uspesno = true;
+ }else{
+   uspesno = false;
+ }
+  return uspesno;
+}
+
+bool rerna::smanjiTemperaturu(){
+  bool uspesno;
+  
+ if(trenutnoStanje = UKLJUCENA && temperatura - KORAK_ZAGREVANJA >= 0){
+   temperatura = temperatura - KORAK_ZAGREVANJA;
+   usoesno = true;
+ }else{
+   uspesno = false;
+ }
+  return uspesno;
+}
+
+trenutnoStanje rerna::getTrenutnoStanje() const{
+  return trenutnoStanje;
+}
+
+int rerna::getTemperatura() const{
+  return temperatura;
 }
